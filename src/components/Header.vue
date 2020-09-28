a747c1b50d1b2987df967e59608adc84a0276be5
Ścibior Arkadiusz <ascibior@kbj.com.pl>
2020-09-28 08:39:26 +0200

    Header: added header to app

---
+++ src/components/Header.vue
@@ -0,0 +1,90 @@
+<template>
+	<div class="navigation">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="logo">
+					<!-- <img alt="Vue logo" src="../assets/logo.png" width="50" height="50"> -->
+				</div>
+			</div>
+
+			<div class="col-md-6">
+				<ul class='menu'>
+					<li v-for="item in menuHeaderItems" v-bind:key="item.id">
+						<span class="menuElement">{{item.name}}</span>
+						<b-icon style="margin-left:5px;" icon="chevron-down"></b-icon>
+					</li>
+				</ul>
+				
+			</div>
+			<div class="col-md-3">
+				<div class="userInfo">
+					<img class='avatar' alt="avatarUser" src="../assets/avatar.jpg">
+					<label for="" class='userName'>Arek Ścibior</label>
+					<b-icon style="margin-left:5px; fill: #fff; cursor: pointer;" icon="gear-fill"></b-icon>
+				</div>
+				
+				<div class="settings">
+
+				</div>
+			</div>	
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Header',
+	props: {
+		msg: String
+	},
+	data() {
+		return { 
+			menuHeaderItems: [
+				{id:'0', name: 'Płatności', hasSub: true, sub: ['Przelew', 'Lista zdefiniowanych kontaktów', 'Zlecenia stałe']},
+				{id:'1', name: 'Karty', hasSub: true, sub: ['Moje karty']},
+				{id:'2', name: 'Kontakt', hasSub: false}
+			]
+		}
+	} 
+}
+</script>
+
+<style>
+	.navigation {
+		background-color: #C60321;
+		opacity: 0.85;
+		height: 80px;
+	}
+	.menu {
+		display: inline-flex;
+		margin: 0;
+		line-height: 80px;
+	}
+	.menu li {
+		margin: 0px 60px 0 0;
+		text-decoration: none;
+		list-style-type: none;
+		cursor: pointer;
+		color: #fff;
+		font-size: 20px;
+
+	}
+	/* .menuElement {
+		
+	} */
+	.menuElement:hover {
+		border-bottom: 2px solid #fff;
+	}
+	.userInfo {line-height: 80px;}
+	.avatar {
+		width: 40px;
+		height: 40px;
+		border-radius: 50%;
+		cursor: pointer;
+	}
+	.userName {
+		color: #fff;
+		font-weight: bold;
+		padding-left: 10px;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// import {Bootstrap, IconsPlugin } from 'boostrap-vue'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
